8d48818111fbeb2554253ff4dc0a618dec9d5476
Matthias Haselmann <haselmann@protonmail.com>
2022-07-03 10:37:34 +0200

    day03: part2 completed

--- day03_binary_diagnostics/answer.py
+++ day03_binary_diagnostics/answer.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 from pathlib import Path
 
 
@@ -30,6 +31,30 @@ def power_consumption(path: Path) -> int:
     return gamma_rate * epsilon_rate
 
 
+def life_support_rating(path: Path) -> int:
+    def get_rating(numbers_str: list[str], filter_most_common: bool) -> int:
+        for bit_idx in range(n_bits):  # only consider the first 5 bits
+            # filt_bit[0] ... filtered bit in case of more error bits
+            filt_bit = ["1", "0"] if filter_most_common else ["0", "1"]
+            bit1_bit0 = 0  # sum of one bits minus sum of 0 bits
+            for number_str in numbers_str:
+                assert number_str[bit_idx] in filt_bit
+                bit1_bit0 += 1 if number_str[bit_idx] == "1" else -1
+            most_common_bit = filt_bit[0] if bit1_bit0 >= 0 else filt_bit[1]
+            numbers_str = [n for n in numbers_str if n[bit_idx] == most_common_bit]
+            if len(numbers_str) == 1:
+                break
+        assert len(numbers_str) == 1
+        return int(numbers_str[0], 2)
+
+    with open(path) as f:
+        numbers_str = f.readlines()
+    n_bits = len(numbers_str[-1])
+    o2_gen_rating = get_rating(copy.copy(numbers_str), True)
+    co2_gen_rating = get_rating(numbers_str, False)
+    return o2_gen_rating * co2_gen_rating
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Advent of Code Day 03: Binary Diagnostic:")
     parser.add_argument("-i", help="Input file path")
@@ -38,3 +63,4 @@ if __name__ == "__main__":
     assert path.exists()
 
     print(f"Power consumption: {power_consumption(path)}")
+    print(f"Live support rating: {life_support_rating(path)}")
