4ec5207072313301ae5c0d2bc4535b63a09e810a
Matthias Haselmann <haselmann@protonmail.com>
2022-07-03 12:54:51 +0200

    day04; part2 solved; refactored

--- day04_giant_squid/answer.py
+++ day04_giant_squid/answer.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 from pathlib import Path
 from typing import Optional
 
@@ -7,27 +8,30 @@ import numpy as np
 
 class BingoBoard:
     def __init__(self, board: list[list[int]]):
-        self.board = np.array(board)
-        assert self.board.shape == (5, 5)
-        self.marked = np.zeros(self.board.shape, dtype=bool)
-        self.last_called_number: Optional[int] = None
+        self._board = np.array(board)
+        assert self._board.shape == (5, 5)
+        self._marked = np.zeros(self._board.shape, dtype=bool)
+        self._last_called_number: Optional[int] = None
 
     def mark(self, number: int):
-        self.last_called_number = number
-        self.marked[self.board == number] = True
+        self._last_called_number = number
+        self._marked[self._board == number] = True
 
     def is_win(self) -> bool:
         # check horizontal and vertical lines
         for idx in range(5):
-            if np.sum(self.marked[:, idx]) == 5 or np.sum(self.marked[idx, :]) == 5:
+            if np.sum(self._marked[:, idx]) == 5 or np.sum(self._marked[idx, :]) == 5:
                 return True
         # check diagonals  (they don't count, lol)
-        # if np.sum(np.diag(self.marked)) == 5 or np.sum(np.diag(np.fliplr(self.marked))) == 5:
+        # if np.sum(np.diag(self._marked)) == 5 or np.sum(np.diag(np.fliplr(self._marked))) == 5:
         #     return True
         return False
 
     def final_score(self) -> int:
-        return np.sum(self.board[~self.marked]) * self.last_called_number
+        return np.sum(self._board[~self._marked]) * self._last_called_number
+
+    def __str__(self):
+        return f"\n{self.__class__}\n{self._board}\n{self._marked}\n{self._last_called_number}\n"
 
 
 def parse_input(path: Path) -> tuple[list[BingoBoard], list[int]]:
@@ -52,9 +56,7 @@ def parse_input(path: Path) -> tuple[list[BingoBoard], list[int]]:
 
 
 # why is it so slow?
-def get_first_winning_board(
-    boards: list[BingoBoard], seq: list[int]
-) -> BingoBoard:  # also return last called number of seq
+def get_first_winning_board(boards: list[BingoBoard], seq: list[int]) -> BingoBoard:
     for number in seq:
         for board in boards:
             board.mark(number)
@@ -63,18 +65,29 @@ def get_first_winning_board(
     raise ValueError(f"Sequence {seq} not long enough for any board to win")
 
 
-if __name__ == "__main__":
-    import time
+def get_last_winning_board(boards: list[BingoBoard], seq: list[int]) -> BingoBoard:
+    boards.reverse()
+    for number in seq:
+        for idx in reversed(range(len(boards))):
+            boards[idx].mark(number)
+            if boards[idx].is_win():
+                if len(boards) == 1:
+                    return boards[idx]
+                else:
+                    del boards[idx]
+    raise ValueError(f"Sequence {seq} not long enough for all boards to win")
 
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Advent of Code Day 03: Binary Diagnostic:")
     parser.add_argument("-i", help="Input file path")
     args = parser.parse_args()
     path = Path(args.i) if args.i else Path("input.txt")
     assert path.exists()
-
-    t0 = time.time()
     boards, seq = parse_input(path)
-    winner_board = get_first_winning_board(boards=boards, seq=seq)
-    t_delta = time.time() - t0
-    print(f"First winning board ({t_delta}s): \n {winner_board.board} \n {winner_board.marked}")
-    print(f"Final score: {winner_board.final_score()}")
+
+    winner_board = get_first_winning_board(copy.deepcopy(boards), seq)
+    print(f"First winning board: {winner_board} with final score {winner_board.final_score()}")
+
+    worst_board = get_last_winning_board(boards, seq)
+    print(f"\n \n Last winning board: {worst_board} with final score {worst_board.final_score()}")
