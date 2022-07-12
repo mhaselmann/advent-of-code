093c54cb4f6d7c320fb8d201567dee9d7b59b3a1
Matthias Haselmann <haselmann@protonmail.com>
2022-07-12 12:42:27 +0200

    day07: part 2 solved

--- day07_the_treachery_of_whales/answer.py
+++ day07_the_treachery_of_whales/answer.py
@@ -1,6 +1,6 @@
 import argparse
-from mimetypes import init
 from pathlib import Path
+from typing import Callable
 
 
 def read_input_file(path: Path) -> list[int]:
@@ -9,11 +9,20 @@ def read_input_file(path: Path) -> list[int]:
     return list(map(int, input.split(",")))
 
 
-def alignment_cost(init_positions: list[int], alignment_position: int) -> int:
+def alignment_cost_part1(init_positions: list[int], alignment_position: int) -> int:
     return sum([abs(i - alignment_position) for i in init_positions])
 
 
-def optimize_alignment_naive(init_positions: list[int]) -> tuple[int, int]:
+def triangular_number(n: int) -> int:
+    return int(n * (n + 1) / 2)
+
+
+def alignment_cost_part2(init_positions: list[int], alignment_position: int) -> int:
+    # pos_delta=[0, 1, 2, 3, 4, 5 ...] --> cost=[0, 1, 3, 6, 10, 15]  --> triangular number :)
+    return sum([triangular_number(abs(i - alignment_position)) for i in init_positions])
+
+
+def optimize_alignment_naive(cost_f: Callable, init_positions: list[int]) -> tuple[int, int]:
     """
     Optimize alignment cost function by trying out all values between min and max position
     Returns: Tuple[alignment_position, cost]
@@ -21,7 +30,7 @@ def optimize_alignment_naive(init_positions: list[int]) -> tuple[int, int]:
     min_pos, max_pos = min(init_positions), max(init_positions)
     costs: dict[int, int] = dict()  # dict[alignment_pos, cost]
     for pos in range(min_pos, max_pos + 1):
-        costs[pos] = alignment_cost(init_positions, pos)
+        costs[pos] = cost_f(init_positions, pos)
     return min(costs.items(), key=lambda x: x[1])
 
 
@@ -33,5 +42,8 @@ if __name__ == "__main__":
     assert path.exists()
 
     init_positions = read_input_file(path)
-    alignment_pos, cost = optimize_alignment_naive(init_positions)
-    print(f"Optimum: Alignment position {alignment_pos}. Required fuel: {cost}")
+    alignment_pos, cost = optimize_alignment_naive(alignment_cost_part1, init_positions)
+    print(f"Optimum Part1: Alignment position {alignment_pos}. Required fuel: {cost}")
+
+    alignment_pos, cost = optimize_alignment_naive(alignment_cost_part2, init_positions)
+    print(f"Optimum Part2: Alignment position {alignment_pos}. Required fuel: {cost}")
