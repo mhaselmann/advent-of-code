38c862a2d4051bb3d26e58ea350f88e95a7d3e47
Matthias Haselmann <haselmann@protonmail.com>
2022-08-05 23:16:13 +0200

    day20, part1 solved

--- 2021/20_trench_map/answer.py
+++ 2021/20_trench_map/answer.py
@@ -28,35 +28,38 @@ def parse_input_file(file_path: Path) -> tuple[ImgEnhAlgorithm, np.ndarray]:
     }, np.array(rows)
 
 
-def enhance_image(img: np.ndarray, img_enh_algorithm: ImgEnhAlgorithm) -> np.ndarray:
-    img = np.pad(img, [(3, 3), (3, 3)], mode="constant", constant_values=False)
-    img_enh = img.copy()
-    for row in range(1, img.shape[0] - 1):
-        for col in range(1, img.shape[1] - 1):
-            img_enh[row, col] = img_enh_algorithm[
-                np_to_tuple(img[row - 1 : row + 2, col - 1 : col + 2])
-            ]
-    # remove zeros at corners
-    for row in range(img_enh.shape[0]):
-        if np.sum(img_enh[row, :]) > 0:
-            if row > 0:
+def enhance_image(
+    img: np.ndarray, img_enh_algorithm: ImgEnhAlgorithm, iters: int = 2
+) -> np.ndarray:
+    zero_to_one = list(img_enh_algorithm.values())[0]
+    for i in range(iters):
+        cv = i % 2 > 0 and zero_to_one
+        print("HERE", i, cv, iters)
+        img = np.pad(img, [(3, 3), (3, 3)], mode="constant", constant_values=cv)
+        img_enh = img.copy()
+        for row in range(1, img.shape[0] - 1):
+            for col in range(1, img.shape[1] - 1):
+                img_enh[row, col] = img_enh_algorithm[
+                    np_to_tuple(img[row - 1 : row + 2, col - 1 : col + 2])
+                ]
+        # remove zeros at corners
+        for row in range(1, img_enh.shape[0] - 1):
+            if np.sum(img_enh[row, :]) > 0:
                 img_enh = img_enh[row:, :]
-            break
-    for row in reversed(range(img_enh.shape[0])):
-        if np.sum(img_enh[row, :]):
-            if row < img_enh.shape[0] - 1:
+                break
+        for row in reversed(range(1, img_enh.shape[0] - 1)):
+            if np.sum(img_enh[row, :]):
                 img_enh = img_enh[: row + 1, :]
-            break
-    for col in range(img_enh.shape[1]):
-        if np.sum(img_enh[:, col]):
-            if col > 0:
+                break
+        for col in range(1, img_enh.shape[1] - 1):
+            if np.sum(img_enh[:, col]):
                 img_enh = img_enh[:, col:]
-            break
-    for col in reversed(range(img_enh.shape[1])):
-        if np.sum(img_enh[:, col]):
-            if col < img_enh.shape[1] - 1:
+                break
+        for col in reversed(range(1, img_enh.shape[1] - 1)):
+            if np.sum(img_enh[:, col]):
                 img_enh = img_enh[:, : col + 1]
-            break
+                break
+        img = img_enh.copy()
     return img_enh
 
 
@@ -69,7 +72,6 @@ if __name__ == "__main__":
 
     iea, img = parse_input_file(file_path)
     print(img.astype(np.uint8))
-    for _ in range(2):
-        img = enhance_image(img, iea)
+    img = enhance_image(img, iea, 2)
     print(img.astype(np.uint8))
     print(f"Answer part 1: How many pixels are lit up after 2 iterations: {img.sum()}")
