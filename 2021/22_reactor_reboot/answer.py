a4b76dac2334d970c8b25827cc7616a088055412
Matthias Haselmann <haselmann@protonmail.com>
2022-08-13 18:41:45 +0200

    addition and subtraction work on example_input below 50

--- 2021/22_reactor_reboot/answer.py
+++ 2021/22_reactor_reboot/answer.py
@@ -1,4 +1,6 @@
 import argparse
+import copy
+from math import prod
 import re
 from pathlib import Path
 
@@ -26,6 +28,90 @@ def parse_input_file(file_path: Path) -> list[tuple[bool, Rectangle]]:
     return rectangles
 
 
+def _relation(x_: Rectangle, y_: Rectangle) -> str:
+    x_enclosing_y = True
+    x_enclosed_in_y = True
+    intersecting = True
+    for x, y in zip(x_, y_):
+        # enclosing
+        if not (x[0] <= y[0] and x[1] >= y[1]):
+            x_enclosing_y = False
+        # enclosed
+        if not (x[0] >= y[0] and x[1] <= y[1]):
+            x_enclosed_in_y = False
+        # intersecting
+        if not (x[0] >= y[0] and x[0] <= y[1] or x[1] >= y[0] and x[1] <= y[1]):
+            intersecting = False
+    if x_enclosing_y and x_enclosed_in_y:
+        return "ident"
+    elif x_enclosing_y:
+        return "enclosing"
+    elif x_enclosed_in_y:
+        return "enclosed"
+    elif intersecting:
+        return "intersect"
+    else:
+        return "no_intersect"
+
+
+def _split(x_: Rectangle, z_: Rectangle) -> list[Rectangle]:
+    """
+    prerequisite: x_ and z_ must interesect!! check if with _relation
+    Returns splitted rectangle along one axis; at maximum one of the splits can still intereset
+    with z_ unless split was performed across last dimension.
+    Outlook: Splitting is repeated as long as no intersecting
+    """
+    x_split = []
+    for dim, (x, z) in enumerate(zip(x_, z_)):  # iterate through all dimensions
+        if x[0] < z[0]:
+            x_split.append([*x_[:dim], (x[0], z[0] - 1), *x_[dim + 1 :]])
+        if x[1] > z[1]:
+            x_split.append([*x_[:dim], (z[1] + 1, x[1]), *x_[dim + 1 :]])
+        # append intersecting part
+        if len(x_split):
+            x_split.append([*x_[:dim], (max(x[0], z[0]), min(x[1], z[1])), *x_[dim + 1 :]])
+            print("\n Splitted: ", "x:", x_, "z:", z_, "split:", x_split, "\n")
+            return x_split
+
+
+def _subtract_old(z_: Rectangle, x_: Rectangle) -> list[Rectangle]:
+    """
+    Subtract intersecting or enclosed rectangle x from rectangle z set by splitting
+    z in multiple rectangles that do not overlap with x; delete self.rectangles
+    that are enclosed in rectangle
+    """
+    relation = _relation(z_, x_)
+    if relation == "no_intersect":
+        return z_
+    elif relation in ["enclosed", "ident"]:
+        return
+    else:
+        print("_____________splitting: ", z_, x_)
+        splits = _split(z_, x_)
+        res = []
+        for s in splits:
+            r = _subtract(s, x_)
+            if r is not None:
+                res.append(r)
+        return res
+
+
+def _subtract(z_: Rectangle, x_: Rectangle, res: list[Rectangle]):
+    """
+    Subtract intersecting or enclosed rectangle x from rectangle z set by splitting
+    z in multiple rectangles that do not overlap with x; delete self.rectangles
+    that are enclosed in rectangle
+    """
+    relation = _relation(z_, x_)
+    if relation == "no_intersect":
+        res.append(z_)
+    elif relation in ["enclosing", "intersect"]:
+        print(relation, "_____________splitting: ", z_, x_)
+        splits = _split(z_, x_)
+        for s in splits:
+            _subtract(s, x_, res)
+
+
 class IntersectingRectangles:
     """
     maintain non-overlapping list of rectangles in self.rectangles
@@ -34,76 +120,46 @@ class IntersectingRectangles:
     def __init__(self):
         self.rectangles: list[Rectangle] = []
 
-    @staticmethod
-    def _relation(x_: Rectangle, y_: Rectangle) -> str:
-        x_enclosing_y = True
-        x_enclosed_in_y = True
-        intersecting = True
-        for x, y in zip(x_, y_):
-            # enclosing
-            if not (x[0] <= y[0] and x[1] >= y[1]):
-                x_enclosing_y = False
-            # enclosed
-            if not (x[0] >= y[0] and x[1] <= y[1]):
-                x_enclosed_in_y = False
-            # intersecting
-            if not (x[0] >= y[0] and x[0] <= y[1] or x[1] >= y[0] and x[1] <= y[1]):
-                intersecting = False
-        if x_enclosing_y and x_enclosed_in_y:
-            return "ident"
-        elif x_enclosing_y:
-            return "enclosing"
-        elif x_enclosed_in_y:
-            return "enclosed"
-        elif intersecting:
-            return "intersect"
-        else:
-            return "non-intersect"
-
-    @staticmethod
-    def _split(x_: Rectangle, y_: Rectangle) -> list[Rectangle]:
-        """
-        prerequisite: x_ and y_ must interesect!! check if with _relation
-        Returns splitted rectangle along one axis; at maximum one of the splits can still intereset
-        with y_ unless split was performed across last dimension.
-        Outlook: Splitting is repeated as long as no intersecting
-        """
-        x_split = []
-        for dim, (x, y) in enumerate(zip(x_, y_)):  # iterate through all dimensions
-            if x[0] < y[0]:
-                x_split.append([*x_[:dim], (x[0], y[0] - 1), *x_[dim + 1 :]])
-            if x[1] > y[1]:
-                x_split.append([*x_[:dim], (y[1] + 1, x[1]), *x_[dim + 1 :]])
-            # append intersecting part
-            if len(x_split):
-                x_split.append([*x_[:dim], (max(x[0], y[0]), min(x[1], y[1])), *x_[dim + 1 :]])
-                print("\n Splitted: ", "x:", x_, "y:", y_, "split:", x_split, "\n")
-                return x_split
-
-    def add(self, rectangle: Rectangle):
+    def add(self, x: Rectangle):
         """
         Add rectangle ot internal set of non-overlapping rectangles by splitting
         up "input rectangle" into multiple rectangles that do not overlap with
         any other preexisting rectangle.
         """
-        new_rectangles = [rectangle]
-        for idx, reference in reversed(list(enumerate(self.rectangles))):
-            relation = self._relation(rectangle, reference)
-            print("\n Relation:", relation, "x:", rectangle, "y:", reference, "\n")
+        for idx, rect in reversed(list(enumerate(self.rectangles))):
+            relation = _relation(x, rect)
+            # print("\n Relation:", relation, "x:", x, "y:", rect, "\n")
             if relation in ["ident", "enclosed"]:  # no further actions required
                 return
             elif relation == "enclosing":  # remove existing rectangle
                 del self.rectangles[idx]
             elif relation == "intersect":  # split up
-                ad_rectangle = False
-                splitted_rectangles = self._split(rectangle, reference)
+                splitted_rectangles = _split(x, rect)
                 for splitted_rectangle in splitted_rectangles:
-                    # print("adding splitted rectangle", splitted_rectangle)
                     self.add(splitted_rectangle)
                 return
-        print("Appending: ", rectangle)
-        self.rectangles.append(rectangle)
+        # print("Appending: ", x)
+        self.rectangles.append(x)
         print(self.rectangles)
+
+    def subtract(self, x: Rectangle):
+        """
+        Subtract rectangle from self.rectangles set by splitting self.rectangles in rectangles
+        that do not overlap with rectangle; delete self.rectangles that are enclosed in rectangle
+        """
+        new_rectangles = []
+        for r in self.rectangles:
+            _subtract(r, x, new_rectangles)
+        self.rectangles = new_rectangles
+
+    def sum(self):
+        sum = 0
+        for r_ in self.rectangles:
+            side_lengths = []
+            for r in r_:
+                side_lengths.append(r[1] - r[0] + 1)
+            sum += prod(side_lengths)
+        return sum
 
 
 if __name__ == "__main__":
@@ -116,7 +172,7 @@ if __name__ == "__main__":
     rectangles = parse_input_file(file_path)
     print(rectangles)
 
-    print(IntersectingRectangles._relation(rectangles[3][0], rectangles[0][0]))
+    print(_relation(rectangles[3][0], rectangles[0][0]))
 
     intersecting_rectangles = IntersectingRectangles()
     intersecting_rectangles.add(rectangles[3][0])
@@ -124,3 +180,21 @@ if __name__ == "__main__":
     intersecting_rectangles.add(rectangles[1][0])
     intersecting_rectangles.add(rectangles[2][0])
     print(intersecting_rectangles.rectangles)
+
+    print(_split(rectangles[0][0], rectangles[3][0]))
+    print("\n \n")
+    res = []
+    print("HERE", _subtract(rectangles[0][0], rectangles[3][0], res))
+    print(res)
+
+    intersecting_rectangles = IntersectingRectangles()
+    intersecting_rectangles.add(rectangles[0][0])
+    print("first", intersecting_rectangles.sum())
+    intersecting_rectangles.add(rectangles[1][0])
+    print("second", intersecting_rectangles.sum())
+    intersecting_rectangles.subtract(rectangles[2][0])
+    print("third", intersecting_rectangles.sum())
+    intersecting_rectangles.add(rectangles[3][0])
+    print("forth", intersecting_rectangles.sum())
+    print("\n \n", intersecting_rectangles.rectangles)
+    print(intersecting_rectangles.sum())
